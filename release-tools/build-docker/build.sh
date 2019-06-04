b84d1abfdcd2a482c1f5a1e2dae545b0b2c68605
雄郷 覚良 <ozato.tadayoshi@wing.sysrdc.com>
2019-06-04 14:22:25 +0900

    developコンテナの中身のバージョン表示はgitのshaにするように

--- release-tools/build-docker/build.sh
+++ release-tools/build-docker/build.sh
@@ -1,14 +1,15 @@
 #/bin/bash
 
 readonly GIT_TAG=$(git tag --points-at HEAD)
-readonly VERSION=${GIT_TAG:-"develop"}
+readonly TAG=${GIT_TAG:-"develop"}
+readonly VERSION=${GIT_TAG:-"develop:-`git rev-parse HEAD`}
 readonly SCRIPT_DIR=$(cd $(dirname $0); pwd)
 readonly REPO_ROOT_DIR="$SCRIPT_DIR/../.."
 
 main(){
-    docker build --network host -f $SCRIPT_DIR/web-api-Dockerfile -t kamonohashi/web-api:$VERSION --build-arg version=$VERSION web-api
-    docker build --network host -f $SCRIPT_DIR/cli-Dockerfile -t kamonohashi/cli:$VERSION --build-arg version=$VERSION .
-    docker build --network host -f $SCRIPT_DIR/web-pages-Dockerfile -t kamonohashi/web-pages:$VERSION --build-arg version=$VERSION web-pages
+    docker build --network host -f $SCRIPT_DIR/web-api-Dockerfile -t kamonohashi/web-api:$TAG --build-arg version=$VERSION web-api
+    docker build --network host -f $SCRIPT_DIR/cli-Dockerfile -t kamonohashi/cli:$TAG --build-arg version=$VERSION .
+    docker build --network host -f $SCRIPT_DIR/web-pages-Dockerfile -t kamonohashi/web-pages:$TAG --build-arg version=$VERSION web-pages
 }
 
 # このスクリプトをどこから実行しても大丈夫なように
