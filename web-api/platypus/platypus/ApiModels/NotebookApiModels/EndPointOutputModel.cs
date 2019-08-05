f86e61eda4d8fdc3c444492055df733877f4d0ed
Reiw98 <Reiw98@users.noreply.github.com>
2019-08-05 17:34:09 +0900

    再起動時にステータスチェックを追加+モデルクラスの修正

--- web-api/platypus/platypus/ApiModels/NotebookApiModels/EndPointOutputModel.cs
+++ web-api/platypus/platypus/ApiModels/NotebookApiModels/EndPointOutputModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Nssol.Platypus.ApiModels.NotebookApiModels
+﻿namespace Nssol.Platypus.ApiModels.NotebookApiModels
 {
     /// <summary>
     /// ノートブックアクセス用URLの出力モデル
