f7cbafe443ae0f5a93b46b9b4ec249bb43f3f3bc
Kaori Mikuni <mikuni.kaori.2z7@jp.nssol.nipponsteel.com>
2019-08-06 16:00:12 +0900

    notebook詳細の実行者を作成者に修正

--- web-pages/src/components/notebook/Edit.vue
+++ web-pages/src/components/notebook/Edit.vue
@@ -45,7 +45,7 @@
               </div>
             </el-form-item>
 
-            <pl-display-text-form label="実行者" :value="createdBy"/>
+            <pl-display-text-form label="作成者" :value="createdBy"/>
             <pl-display-text-form label="開始日時" :value="createdAt"/>
             <pl-display-text-form label="完了日時" :value="completedAt"/>
             <pl-display-text-form label="待機時間" :value="waitingTime"/>
