6f7d2b7ebac69c6cf996243612a6aaa87e662098
Kaori Mikuni <mikuni.kaori.2z7@jp.nssol.nipponsteel.com>
2019-08-08 10:34:35 +0900

    dashboardをもとに戻した

--- web-pages/src/components/dashboard/Index.vue
+++ web-pages/src/components/dashboard/Index.vue
@@ -2,97 +2,23 @@
   <div>
     <h2> ダッシュボード </h2>
     <div class="dashboard">
-      <el-card class="box-card" shadow="Always">
-        <div slot="header">
-          <span>Workflow</span>
-        </div>
-        <div>
-          <el-row>
-            <el-col :xs="24" :sm="6">
-              <div class="menu-name">
-                <img class="logo" src="/static/images/illust_folder2_bl.png" alt="" width="160" height="135">
-                <br/>
-                {{'データ準備' }}
-              </div>
-              <div class=menu-description style="padding:10px;font-size:14px;">{{'学習で使用するデータの準備を行います'}}</div>
-              <router-link :to='"/data"'>
-                <el-row>
-                  <div class="menu-item" style="padding:10px;font-size:16px;">
-                    {{'データアップロード'}}
-                  </div>
-                </el-row>
-              </router-link>
-              <router-link :to='"/preprocessing"'>
-                <el-row>
-                  <div class="menu-item" style="padding:10px;font-size:16px;">
-                    {{'前処理実行'}}
-                  </div>
-                </el-row>
-              </router-link>
-              <router-link :to='"/dataset"'>
-                <el-row>
-                  <div class="menu-item" style="padding:10px;font-size:16px;">
-                    {{'データセット作成'}}
-                  </div>
-                </el-row>
-              </router-link>
-            </el-col>
-            <el-col :xs="24" :sm="6">
-              <div class="menu-name">
-                <img class="logo" src="/static/images/illust_book3.png" alt="" width="160" height="135">
-                <br>
-                {{'note book' }}
-              </div>
-              <div class=menu-description style="padding:10px;font-size:14px;">{{'jupyter上で試行錯誤ができます'}}</div>
-
-              <router-link :to='"/notebook"'>
-                <el-row>
-                  <div class="menu-item" style="padding:10px;font-size:16px;">
-                    {{'Jupyter Lab'}}
-                    <br>
-                  </div>
-                </el-row>
-              </router-link>
-            </el-col>
-            <el-col :xs="24" :sm="6">
-              <div class="menu-name">
-                <img class="logo" src="/static/images/illust_neuralBrain_bl.png" alt="" width="160" height="135">
-                <br>
-                {{'学習'}}
-              </div>
-              <div class=menu-description style="padding:10px;font-size:14px;">{{'モデルを訓練し調整します'}}</div>
-              <router-link :to='"/training"'>
-                <el-row>
-                  <div class="menu-item" style="padding:10px;font-size:16px;">
-                    {{'学習実行'}}
-                    <br>
-                  </div>
-                </el-row>
-              </router-link>
-            </el-col>
-            <el-col :xs="24" :sm="6">
-              <div class="menu-name">
-                <img class="logo" src="/static/images/illust_machineLearning_bl.png" alt="" width="160" height="135">
-                <br>
-                {{'推論' }}
-              </div>
-              <div class=menu-description style="padding:10px;font-size:14px;">{{'学習結果を用いて推論処理を行います'}}</div>
-              <router-link :to='"/inference"'>
-                <el-row>
-                  <div class="menu-item" style="padding:10px;font-size:16px;">
-                    {{'推論実行'}}
-                    <br>
-                  </div>
-                </el-row>
-              </router-link>
-            </el-col>
-          </el-row>
-        </div>
-      </el-card>
+      <div class="dashboard-menu-list card-container" v-for="(menu, index) in menuList" :key="index">
+        <router-link :to=menu.url>
+          <el-card class=menu
+                   style="border:solid 1px #ebeef5; width:360px;height: 200px; border-left: 5px solid rgba(26, 191, 213, 0.7);">
+            <div class=menu-name>
+              <icon v-if="menu.category" :name="menu.category" scale="1.5" class="menu-icon"></icon>
+              {{menu.name}}
+            </div>
+            <div class=menu-description style="padding:10px;font-size:14px;">{{menu.description }}</div>
+          </el-card>
+        </router-link>
+      </div>
     </div>
     <div class="footer">
       <span class="footer-content">© 2016-2019 NS Solutions Corporation, All Rights Reserved.</span>
     </div>
+
   </div>
 </template>
 <script>
@@ -136,6 +62,17 @@
   }
 </script>
 <style lang="scss" scoped>
+
+  a {
+    text-decoration: none;
+  }
+
+  .menu {
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
+
   .menu-icon {
     position: relative;
     top: 5px;
@@ -174,27 +111,5 @@
     position: fixed;
     bottom: 0;
     padding: 10px;
-  }
-
-  .box-card {
-    width: 100%;
-    margin-bottom: 20px;
-  }
-  a {
-    color: #303133;
-    text-decoration: none;
-  }
-
-  .menu-item {
-    border: solid 1px #EBEEF5;
-    padding: 2% 2% 2% 3%;
-    margin: 10px 0px;
-    overflow: hidden;
-    width: 80%;
-    max-width: 240px;
-    border-radius: 4px;
-  }
-  .menu-item:hover {
-    border-left: solid 5px #1abfd5;
   }
 </style>
