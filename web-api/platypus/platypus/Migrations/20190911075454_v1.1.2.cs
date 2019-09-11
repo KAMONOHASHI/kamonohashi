782ed78f3be829e401061175d66fcc294105114b
Reiw98 <Reiw98@users.noreply.github.com>
2019-09-11 16:59:47 +0900

    ユーザとテナントの紐づけを外した際の処理を修正

---
+++ web-api/platypus/platypus/Migrations/20190911075454_v1.1.2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Text;
+
+namespace Nssol.Platypus.Migrations
+{
+    public partial class v112 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            StringBuilder query = new StringBuilder();
+
+            // UserTenantGitMaps テーブルから不要データを削除するクエリを作成
+            query.AppendLine("DELETE FROM \"UserTenantGitMaps\"");
+            query.AppendLine("WHERE");
+            query.AppendLine("	\"Id\" IN (");
+            query.AppendLine("		SELECT UTGM.\"Id\"");
+            query.AppendLine("		FROM \"UserTenantGitMaps\" AS UTGM");
+            query.AppendLine("		INNER JOIN \"TenantGitMaps\" AS TGM");
+            query.AppendLine("		ON UTGM.\"TenantGitMapId\" = TGM.\"Id\"");
+            query.AppendLine("		WHERE");
+            query.AppendLine("			NOT EXISTS (");
+            query.AppendLine("				SELECT * ");
+            query.AppendLine("				FROM \"UserTenantMaps\" AS UTM");
+            query.AppendLine("				WHERE TGM.\"TenantId\" = UTM.\"TenantId\"");
+            query.AppendLine("				AND UTM.\"UserId\" = UTGM.\"UserId\")");
+            query.AppendLine("	);");
+
+            migrationBuilder.Sql(query.ToString());
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
