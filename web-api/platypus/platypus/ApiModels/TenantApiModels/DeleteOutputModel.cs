a0f601b7a5b825174ad4f46f42fce7d1bf57b26e
Reiw98 <Reiw98@users.noreply.github.com>
2020-01-28 11:27:54 +0900

    不要using削除

--- web-api/platypus/platypus/ApiModels/TenantApiModels/DeleteOutputModel.cs
+++ web-api/platypus/platypus/ApiModels/TenantApiModels/DeleteOutputModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Nssol.Platypus.ApiModels.TenantApiModels
+﻿namespace Nssol.Platypus.ApiModels.TenantApiModels
 {
     public class DeleteOutputModel
     {
