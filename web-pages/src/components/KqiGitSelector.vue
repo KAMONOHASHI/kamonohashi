e74158d0cec49b13a6059b3cc4e87994382fa7a0
hama1080 <12245259+hama1080@users.noreply.github.com>
2020-02-12 17:49:13 +0900

    git選択用コンポーネントの作成

---
+++ web-pages/src/components/KqiGitSelector.vue
@@ -0,0 +1,249 @@
+<!--name: 学習モデルのGit情報の入力コンポーネント,-->
+<!--description: リポジトリ名、ブランチ名を指定するドロップダウンをそれぞれ表示する。そのままだとHEADになるので、任意でコミットIDも直接指定可能,-->
+
+<template>
+  <div class="el-input">
+    <el-row>
+      <!-- サーバの選択 -->
+      <el-col :span="6" :offset="1">Gitサーバ</el-col>
+      <el-col :span="12">
+        <el-select
+          :value="git"
+          size="small"
+          filterable
+          value-key="id"
+          clearable
+          remote
+          :disabled="disabled"
+          @change="changeGit"
+        >
+          <el-option
+            v-for="item in gits"
+            :key="item.id"
+            :label="item.name"
+            :value="item"
+          >
+          </el-option>
+        </el-select>
+      </el-col>
+    </el-row>
+    <el-row>
+      <!-- リポジトリの選択 -->
+      <el-col :span="6" :offset="1">リポジトリ</el-col>
+      <el-col :span="12">
+        <el-select
+          :value="repository"
+          size="small"
+          filterable
+          clearable
+          allow-create
+          default-first-option
+          remote
+          :value-key="repositoryValueKey"
+          :disabled="!git || loadingRepositories || disabled"
+          @change="changeRepository"
+        >
+          <el-option
+            v-for="item in repositories"
+            :key="item.fullName"
+            :label="item.fullName"
+            :value="item"
+          >
+          </el-option>
+        </el-select>
+      </el-col>
+      <el-col :span="2">
+        <i v-if="loadingRepositories" class="el-icon-loading"></i>
+      </el-col>
+    </el-row>
+    <el-row>
+      <!-- ブランチの選択 -->
+      <el-col :span="6" :offset="1">ブランチ</el-col>
+      <el-col :span="12">
+        <el-select
+          :value="branch"
+          size="small"
+          filterable
+          clearable
+          remote
+          default-first-option
+          :disabled="!repository || disabled"
+          :loading="listLoading"
+          value-key="branchName"
+          @change="changeBranch"
+        >
+          <el-option
+            v-for="item in branches"
+            :key="item.branchName"
+            :label="item.branchName"
+            :value="item"
+          >
+          </el-option>
+        </el-select>
+      </el-col>
+    </el-row>
+    <el-row>
+      <!-- コミットIDの選択。ブランチで選択する場合は表示されない。 -->
+      <el-col :span="6" :offset="1">コミットID</el-col>
+      <el-col v-if="enableCommitIdSelecter" :span="12">
+        <el-popover
+          ref="commitDetail"
+          :disabled="commit === null"
+          title="コミット詳細"
+          trigger="hover"
+          width="350"
+          placement="right"
+        >
+          <span>
+            <kqi-display-text-form
+              label="コミットID"
+              :value="commit ? commit.commitId : ''"
+            ></kqi-display-text-form>
+            <kqi-display-text-form
+              label="コミッター"
+              :value="commit ? commit.committerName : ''"
+            ></kqi-display-text-form>
+            <kqi-display-text-form
+              label="コミット日時"
+              :value="commit ? commit.commitAt : ''"
+            ></kqi-display-text-form>
+            <kqi-display-text-form
+              label="コメント"
+              :value="commit ? commit.comment : ''"
+            ></kqi-display-text-form>
+          </span>
+        </el-popover>
+        <el-select
+          v-popover:commitDetail
+          :value="commit"
+          size="small"
+          filterable
+          clearable
+          remote
+          default-first-option
+          :disabled="!branch || disabled"
+          :loading="listLoading"
+          value-key="commitId"
+          @change="changeCommit"
+        >
+          <!-- 選択解除用 -->
+          <el-option key="HEAD" label="HEAD" :value="null" />
+          <el-option
+            v-for="item in commits"
+            :key="item.commitId"
+            :label="item.commitId"
+            :value="item"
+          >
+          </el-option>
+        </el-select>
+      </el-col>
+      <el-col v-else :span="12">
+        <span>
+          HEAD
+        </span>
+        <el-button
+          size="mini"
+          :disabled="!branch || disabled"
+          @click="enableCommitIdSelecter = true"
+        >
+          コミットIDを指定
+        </el-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import KqiDisplayTextForm from '@/components/KqiDisplayTextForm'
+import { createNamespacedHelpers } from 'vuex'
+const { mapGetters } = createNamespacedHelpers('gitSelector')
+
+export default {
+  components: {
+    KqiDisplayTextForm,
+  },
+
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      model: this.value === undefined || this.value === null ? {} : this.value,
+
+      // popover（コミットID一覧等）の「ローディング中」 文字列の表示制御
+      listLoading: false,
+      enableCommitIdSelecter: false,
+      // リポジトリ名が手入力されたかどうかを表すフラグ
+      repositoryCreated: false,
+      repositoryValueKey: 'fullName',
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'gits',
+      'repositories',
+      'branches',
+      'commits',
+      'git',
+      'repository',
+      'branch',
+      'commit',
+      'loadingRepositories',
+    ]),
+  },
+
+  methods: {
+    changeGit(git) {
+      if (git === '') {
+        // clearボタンが押下された場合
+        this.$emit('input', { type: 'git', value: null })
+      } else {
+        this.$emit('input', { type: 'git', value: git })
+      }
+    },
+
+    // 選択しているリポジトリが切り替わった時に呼ばれるイベントハンドラ。
+    changeRepository(repository) {
+      if (repository === '') {
+        // clearボタンが押下された場合
+        this.$emit('input', { type: 'repository', value: null })
+      } else {
+        this.$emit('input', { type: 'repository', value: repository })
+      }
+
+      if (typeof repository === 'string') {
+        // リポジトリ名を手入力された
+        this.repositoryValueKey = 'value'
+      } else {
+        this.repositoryValueKey = 'fullName'
+      }
+    },
+
+    // 選択しているブランチが切り替わった時に呼ばれるイベントハンドラ。
+    changeBranch(branch) {
+      if (branch === '') {
+        // clearボタンが押下された場合
+        this.$emit('input', { type: 'branch', value: null })
+      } else {
+        this.$emit('input', { type: 'branch', value: branch })
+      }
+    },
+
+    // 選択しているコミットが切り替わった時に呼ばれるイベントハンドラ。
+    changeCommit(commit) {
+      if (commit === '') {
+        // clearボタンが押下された場合
+        this.$emit('input', { type: 'commit', value: null })
+      } else {
+        this.$emit('input', { type: 'commit', value: commit })
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ web-pages/src/store/modules/gitSelector.js
@@ -0,0 +1,157 @@
+import api from '@/api/v1/api'
+
+// initial state
+const state = {
+  gits: [],
+  repositories: [],
+  branches: [],
+  commits: [],
+
+  git: null,
+  repository: null,
+  branch: null,
+  commit: null,
+
+  // リポジトリのロードは時間がかかる可能性があるためフラグを設けて管理
+  loadingRepositories: false,
+}
+
+// getters
+const getters = {
+  gits(state) {
+    return state.gits
+  },
+  repositories(state) {
+    return state.repositories
+  },
+  branches(state) {
+    return state.branches
+  },
+  commits(state) {
+    return state.commits
+  },
+
+  git(state) {
+    return state.git
+  },
+  repository(state) {
+    return state.repository
+  },
+  branch(state) {
+    return state.branch
+  },
+  commit(state) {
+    return state.commit
+  },
+
+  loadingRepositories(state) {
+    return state.loadingRepositories
+  },
+}
+
+// actions
+const actions = {
+  async fetchGits({ commit }) {
+    let response = (await api.account.getGits()).data
+    let gits = response.gits
+    let defaultGitId = response.defaultGitId
+    commit('setGits', { gits })
+
+    commit(
+      'setGit',
+      gits.find(git => {
+        return git.id === defaultGitId
+      }),
+    )
+  },
+
+  async fetchRepositories(context) {
+    let gitId = context.state.git.id
+    context.commit('setLoadingRepositories', true)
+    let repositories = (await api.git.getRepos({ gitId: gitId })).data
+    context.commit('setRepositories', { repositories })
+    context.commit('setLoadingRepositories', false)
+  },
+
+  async fetchBranches(context) {
+    if (typeof context.state.repository === 'string') {
+      // リポジトリ名を手入力された
+      let repositoryName = context.state.repository
+      let index = repositoryName.indexOf('/')
+
+      if (index > 0) {
+        let repository = {
+          owner: repositoryName.substring(0, index),
+          name: repositoryName.substring(index + 1),
+          fullName: repositoryName,
+        }
+        context.commit('setRepository', repository)
+        // リポジトリ一覧に追加
+        let repositories = context.state.repositories
+        repositories.push(repository)
+        context.commit('setRepositories', { repositories })
+      } else {
+        //構文エラー
+      }
+    }
+    let params = {
+      gitId: context.state.git.id,
+      owner: context.state.repository.owner,
+      repositoryName: context.state.repository.name,
+    }
+    let branches = (await api.git.getBranches(params)).data
+    context.commit('setBranches', { branches })
+  },
+
+  async fetchCommits(context) {
+    let params = {
+      gitId: context.state.git.id,
+      owner: context.state.repository.owner,
+      repositoryName: context.state.repository.name,
+      branch: context.state.branch.name,
+    }
+    let commits = (await api.git.getCommits(params)).data
+    context.commit('setCommits', { commits })
+  },
+}
+
+// mutations
+const mutations = {
+  setGits(state, { gits }) {
+    state.gits = gits
+  },
+  setRepositories(state, { repositories }) {
+    state.repositories = repositories
+  },
+  setBranches(state, { branches }) {
+    state.branches = branches
+  },
+  setCommits(state, { commits }) {
+    state.commits = commits
+  },
+
+  setGit(state, git) {
+    state.git = git
+  },
+  setRepository(state, repository) {
+    state.repository = repository
+  },
+  setBranch(state, branch) {
+    state.branch = branch
+  },
+  setCommit(state, commit) {
+    state.commit = commit
+  },
+
+  setLoadingRepositories(state, flag) {
+    state.loadingRepositories = flag
+  },
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+}
