7bb1cf98ba8a16dd823969bf491daa9b3340020b
hama1080 <12245259+hama1080@users.noreply.github.com>
2020-02-14 11:58:40 +0900

    trainingのvuex更新

--- web-pages/src/store/modules/training.js
+++ web-pages/src/store/modules/training.js
@@ -5,7 +5,9 @@ const state = {
   histories: [],
   total: 0,
   selections: [],
-  detail: {},
+  detail: null,
+  events: {},
+  uploadedFiles: {},
   partitions: [],
 }
 
@@ -22,6 +24,12 @@ const getters = {
   },
   detail(state) {
     return state.detail
+  },
+  events(state) {
+    return state.events
+  },
+  uploadedFiles(state) {
+    return state.uploadedFiles
   },
   partitions(state) {
     return state.partitions
@@ -43,6 +51,21 @@ const actions = {
     commit('setDetail', { detail })
   },
 
+  async fetchEvents({ commit }, id) {
+    let events = (await api.training.getEventsById({ id: id })).data
+    commit('setEvents', { events })
+  },
+
+  async fetchUploadedFiles({ commit }, id) {
+    let uploadedFiles = (
+      await api.training.getFilesById({
+        id: id,
+        withUrl: true,
+      })
+    ).data
+    commit('setUploadedFiles', { uploadedFiles })
+  },
+
   async fetchPartitions({ commit }) {
     let partitions = (await api.cluster.getPartitions()).data
     commit('setPartitions', { partitions })
@@ -54,8 +77,42 @@ const actions = {
   },
 
   // eslint-disable-next-line no-unused-vars
+  async put({ rootState }, params) {
+    return await api.training.putById(params)
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  async postHalt({ state }, id) {
+    return await api.training.postHaltById({ id: id })
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  async postUserCancel({ state }, id) {
+    return await api.training.postUserCancelById({ id: id })
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  async postFiles({ state }, { id, fileInfo }) {
+    for (let i = 0; i < fileInfo.length; i++) {
+      fileInfo[i].FileName = fileInfo[i].name
+      await api.training.postFilesById({
+        id: id,
+        model: fileInfo[i],
+      })
+    }
+  },
+
+  // eslint-disable-next-line no-unused-vars
   async delete({ state }, id) {
     await api.training.deleteById({ id: id })
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  async deleteFile({ state }, { id, fileId }) {
+    await api.training.deleteByIdFilesByFileId({
+      id: id,
+      fileId: fileId,
+    })
   },
 }
 
@@ -64,18 +121,31 @@ const mutations = {
   setHistories(state, { histories }) {
     state.histories = histories
   },
+
   setTotal(state, total) {
     state.total = total
   },
+
   setSelections(state, selections) {
     state.selections = selections
   },
+
   setDetail(state, { detail }) {
     state.detail = detail
   },
+
+  setEvents(state, { events }) {
+    state.events = events
+  },
+
+  setUploadedFiles(state, { uploadedFiles }) {
+    state.uploadedFiles = uploadedFiles
+  },
+
   clearDetail(state) {
     state.detail = {}
   },
+
   setPartitions(state, { partitions }) {
     state.partitions = partitions
   },
