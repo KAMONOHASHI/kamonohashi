25756c6370c9ef5527cb91187b9c8508a3965314
hama1080 <12245259+hama1080@users.noreply.github.com>
2020-02-26 14:12:43 +0900

    dataのmodule実装

--- web-pages/src/store/modules/data.js
+++ web-pages/src/store/modules/data.js
@@ -4,6 +4,8 @@ import api from '@/api/v1/api'
 const state = {
   data: {},
   detail: {},
+  uploadedFiles: [],
+  tags: [],
 }
 
 // getters
@@ -14,6 +16,14 @@ const getters = {
 
   detail(state) {
     return state.detail
+  },
+
+  uploadedFiles(state) {
+    return state.uploadedFiles
+  },
+
+  tags(state) {
+    return state.tags
   },
 }
 
@@ -32,6 +42,55 @@ const actions = {
       commit('setDetail', { detail })
     }
   },
+
+  async fetchDataTags({ commit }) {
+    let tags = (await api.data.getDataTags()).data
+    commit('setTags', tags)
+  },
+
+  async fetchUploadedFiles({ commit }, id) {
+    let uploadedFiles = (
+      await api.data.getFilesById({
+        id: id,
+        withUrl: true,
+      })
+    ).data
+    commit('setUploadedFiles', { uploadedFiles })
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  async post({ state }, params) {
+    return await api.data.post({ model: params })
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  async put({ rootState }, params) {
+    return await api.data.putById(params)
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  async putFile({ rootState }, { id, fileInfo }) {
+    for (let i = 0; i < fileInfo.length; i++) {
+      fileInfo[i].FileName = fileInfo[i].name
+      await api.data.putFilesById({
+        id: id,
+        model: fileInfo[i],
+      })
+    }
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  async delete({ state }, id) {
+    await api.data.deleteById({ id: id })
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  async deleteFile({ rootState }, { id, fileId }) {
+    await api.data.deleteFilesById({
+      id: id,
+      fileId: fileId,
+    })
+  },
 }
 
 // mutations
@@ -46,6 +105,14 @@ const mutations = {
 
   clearDetail(state) {
     state.detail = {}
+  },
+
+  setUploadedFiles(state, { uploadedFiles }) {
+    state.uploadedFiles = uploadedFiles
+  },
+
+  setTags(state, tags) {
+    state.tags = tags
   },
 }
 
