1957ce53b5cbe63eae46767ba9e4873878e73c57
428s <yotsuya.shogo.za2@jp.nssol.nipponsteel.com>
2020-03-04 19:05:51 +0900

    KqiSmartSearchInputの作成

---
+++ web-pages/src/components/KqiSmartSearchInput/InputDate.vue
@@ -0,0 +1,133 @@
+<template>
+  <span>
+    <el-date-picker
+      ref="saveTagInput"
+      v-model="value"
+      type="date"
+      placeholder="Please input"
+      size="mini"
+      format="yyyy-MM-dd"
+      style="width: 150px"
+      @change="handleChange"
+      @blur="handleBlur"
+    />
+
+    <el-popover v-model="show" trigger="manual">
+      <el-table
+        :data="tableData"
+        :show-header="false"
+        @current-change="handleCommand"
+      >
+        <el-table-column prop="name" width="100" />
+        <el-table-column prop="name" width="100" align="right">
+          <template slot-scope="scope">
+            <span style="color:grey;font-size:85%;">{{
+              scope.row.detail
+            }}</span>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-popover>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      value: null,
+      show: false,
+      tableData: [
+        // e.g) 2018/10/1を選択した場合
+        { name: 'のみ', detail: '', symbol: '=' }, // =2018/10/1
+        { name: '以降', detail: '', symbol: '>=', days: 0 }, // >=2018/10/1
+        { name: '以前', detail: '', symbol: '<', days: 1 }, // <2018/10/2
+        { name: 'より後', detail: '', symbol: '>=', days: 1 }, // >=2018/10/2
+        { name: 'より前', detail: '', symbol: '<', days: 0 }, // <2018/10/1
+      ],
+    }
+  },
+  created() {
+    /* eslint-disable */
+    this.$nextTick(_ => {
+      this.value = this.getValue()
+      setTimeout(() => {
+        this.$refs.saveTagInput.focus()
+      }, 600)
+    })
+    /* eslint-enable */
+  },
+  methods: {
+    // 検索する基準値入力後に検索範囲のドロップダウンを表示
+    handleChange() {
+      this.show = true
+    },
+
+    // 検索範囲の設定
+    handleCommand(row) {
+      if (this.value) {
+        this.show = false
+        let value = row.symbol + this.getNowYMD(this.value, row.days)
+        let display = this.getNowYMD(this.value)
+        let suffix = row.name
+        this.emitDone(value, display, suffix)
+      } else {
+        this.emitCancel()
+      }
+    },
+
+    // フォーカスアウト時に値が何も入力されていない場合検索タグを消す
+    handleBlur() {
+      setTimeout(() => {
+        if (!this.value) {
+          this.emitCancel()
+        }
+      }, 300)
+    },
+
+    // 入力された値の整形
+    getNowYMD(date, days) {
+      let dt = new Date(date.valueOf())
+      if (days) {
+        dt.setDate(dt.getDate() + days)
+      }
+
+      var y = dt.getFullYear()
+      if (y >= 10000) {
+        // 10000年以上の場合、最大日付を返す
+        return '9999/12/31'
+      }
+      var m = ('00' + (dt.getMonth() + 1)).slice(-2)
+      var d = ('00' + dt.getDate()).slice(-2)
+      var result = y + '/' + m + '/' + d
+      return result
+    },
+
+    // 検索タグに表示する値の設定
+    getValue() {
+      if (this.tag.display) {
+        return this.tag.display
+      }
+      if (this.tag.option && this.tag.option.default) {
+        return this.tag.option.default
+      }
+      return ''
+    },
+
+    emitDone(value, display, suffix) {
+      this.$emit('done', { value, display, suffix })
+    },
+    emitCancel() {
+      this.$emit('cancel')
+    },
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ web-pages/src/components/KqiSmartSearchInput/InputSelect.vue
@@ -0,0 +1,122 @@
+<template>
+  <span>
+    <el-input
+      ref="saveTagInput"
+      v-model="value"
+      placeholder="Please input"
+      size="mini"
+      style="width:auto;"
+      @blur="handleBlur"
+    />
+
+    <el-popover v-model="show" trigger="manual">
+      <el-table
+        :data="selectData"
+        :show-header="false"
+        @current-change="handleCommand"
+      >
+        <el-table-column prop="label" width="150" />
+        <el-table-column prop="name" width="100" align="right">
+          <template slot-scope="scope">
+            <span style="color:grey;font-size:85%;">{{
+              scope.row.detail
+            }}</span>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-popover>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      value: '',
+      show: true,
+      selectData: [
+        { label: 'テスト1', detail: '詳細１', value: '1' },
+        { label: 'テスト2', detail: '詳細２', value: '2' },
+        { label: 'テスト3', detail: '詳細３', value: '3' },
+        { label: 'テスト4', detail: '詳細４', value: '4' },
+      ],
+    }
+  },
+  created() {
+    /* eslint-disable */
+    this.$nextTick(_ => {
+      this.value = this.getValue()
+      this.selectData = this.getSelectData()
+      this.$refs.saveTagInput.$refs.input.focus()
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.select()
+      })
+    })
+    /* eslint-enable */
+  },
+  methods: {
+    // 検索範囲の設定
+    handleCommand(row) {
+      this.show = false
+      this.value = row.label
+      let value = row.value
+      let display = row.label
+      let suffix = ''
+      this.emitDone(value, display, suffix)
+    },
+
+    // フォーカスアウト時に値が何も入力されていない場合検索タグを消す
+    handleBlur() {
+      setTimeout(() => {
+        if (this.value === '') {
+          this.emitCancel()
+        }
+      }, 300)
+    },
+
+    // 検索タグに表示する値の設定
+    getValue() {
+      if (this.tag.display) {
+        return this.tag.display
+      }
+      if (this.tag.option && this.tag.option.default) {
+        return this.tag.option.default
+      }
+      return ''
+    },
+
+    // 表示する検索条件の設定
+    getSelectData() {
+      // なければデフォルト値を設定
+      if (this.tag.config.option.items) {
+        let items = this.tag.config.option.items
+        let ret = []
+        items.forEach(i => {
+          if (typeof i === 'string') {
+            ret.push({ label: i, value: i })
+          } else {
+            ret.push(i)
+          }
+        })
+        return ret
+      }
+      return this.selectData
+    },
+
+    emitDone(value, display, suffix) {
+      this.$emit('done', { value, display, suffix })
+    },
+    emitCancel() {
+      this.$emit('cancel')
+    },
+  },
+}
+</script>
+
+<style scoped></style>
