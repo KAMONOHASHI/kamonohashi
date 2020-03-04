7b1ed3fd844a887adea6c6365e1d7110136acc69
428s <yotsuya.shogo.za2@jp.nssol.nipponsteel.com>
2020-03-04 13:36:17 +0900

    テナントリソース管理修正

--- web-pages/src/views/tenant-manage/resource/Index.vue
+++ web-pages/src/views/tenant-manage/resource/Index.vue
@@ -4,12 +4,7 @@
 
     <br />
     <br />
-    <el-table
-      :data="tenantResourceDetail"
-      class="table pl-index-table"
-      border
-      @row-click="handleEditOpen"
-    >
+    <el-table :data="tenantResourceDetail" class="table pl-index-table" border>
       <el-table-column prop="name" label="コンテナ" width="auto" />
       <el-table-column prop="createdBy" label="ユーザ" width="auto" />
       <el-table-column prop="nodeName" label="ノード" width="auto" />
