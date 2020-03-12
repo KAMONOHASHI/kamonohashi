56559c62bd230d7ea03ec9bbc750d7ef77283dea
hama1080 <12245259+hama1080@users.noreply.github.com>
2020-03-12 18:16:26 +0900

    前処理履歴詳細画面の修正

--- web-pages/src/views/preprocessing/HistoryEdit.vue
+++ web-pages/src/views/preprocessing/HistoryEdit.vue
@@ -1,14 +1,15 @@
 <template>
   <el-dialog
     class="dialog"
-    title="前処理履歴編集"
+    title="前処理履歴詳細"
     :visible="dialogVisible"
     :before-close="handleCancel"
     :close-on-click-modal="false"
   >
     <el-form ref="editForm">
       <kqi-display-error :error="error" />
-      <kqi-display-text-form label="ID" :value="preprocessingId" />
+      <kqi-display-text-form label="前処理履歴ID" :value="preprocessingId" />
+      <kqi-display-text-form label="データID" :value="historyDetail.dataId" />
       <kqi-display-text-form label="データ名" :value="historyDetail.dataName" />
       <kqi-display-text-form
         label="前処理名"
@@ -50,7 +51,7 @@
             icon="el-icon-close"
             @click="handleCancel"
           >
-            キャンセル
+            閉じる
           </el-button>
           <kqi-delete-button
             class="pull-left btn-update"
