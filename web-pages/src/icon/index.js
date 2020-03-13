6a147e5f07b7e6900ad606b9fa5674e7b7643968
hama1080 <12245259+hama1080@users.noreply.github.com>
2020-03-13 14:21:30 +0900

    メニューアイオンpl-からkqi-に変更

--- web-pages/src/icon/index.js
+++ web-pages/src/icon/index.js
@@ -1,12 +1,12 @@
 // プロジェクト固有の設定を読み込み
-import './pl-data'
-import './pl-dataset'
-import './pl-preprocessing'
-import './pl-notebook'
-import './pl-training'
-import './pl-inference'
-import './pl-tenant-setting'
-import './pl-system-setting'
+import './kqi-data'
+import './kqi-dataset'
+import './kqi-preprocessing'
+import './kqi-notebook'
+import './kqi-training'
+import './kqi-inference'
+import './kqi-tenant-setting'
+import './kqi-system-setting'
 
 // icon を全部読み込むと重いので使うもののみ読み込む
 import 'vue-awesome/icons/user'
