65bbcedad9084466ee76aea4222fd0a41fb513f8
ansible <ozota1@username@users.noreply.github.com>
2020-03-23 01:51:03 +0000

    DBを公開する際はnodeportを使用するように
    externalipsはdeepopsデフォルトのipvsモードでは動かない

--- deploy-tools/test/expose-db.sh
+++ deploy-tools/test/expose-db.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-KQI_NODE=$1
-
-get_ip(){
-  local IP_BY_DNS=$(host $1 | awk '/has address/ { print $4 }')
-  if [ -z "$IP_BY_DNS" ]; then
-    # 名前解決した結果からループバックを除き先頭のIPを選択
-    # hostコマンドはhostsを見ない。getentは/etc/nsswitch.confに従って解決する
-    local IP=$(getent hosts $1 | awk '{print $1}' | grep -v 127* | head -1)
-  else
-    local IP=$IP_BY_DNS
-  fi
-  echo $IP
-}
-
-NODE_IP=$(get_ip $1)
-
-
-
 cat<<EOF | kubectl apply -f -
 kind: Service
 apiVersion: v1
@@ -27,9 +9,10 @@ metadata:
 spec:
   selector:
     app: postgres
+  type: NodePort
   ports:
   - port: 5432
-    targetPort: 5432
-  externalIPs: ["$NODE_IP"]
-
+    nodePort: 35432
 EOF
+
+echo "use port 35432"
