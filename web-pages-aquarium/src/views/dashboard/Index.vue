d55efb934e80634732a1011493b4aed8c9501276
ota.hiroko <ota.hiroko.53d@jp.nssol.nipponsteel.com>
2020-11-19 10:55:27 +0900

    ダッシュボード作成

--- web-pages-aquarium/src/views/dashboard/Index.vue
+++ web-pages-aquarium/src/views/dashboard/Index.vue
@@ -1,24 +1,19 @@
 <template>
   <div>
     <h2>アクアリウムダッシュボード</h2>
-    <div class="dashboard">
+    <div>新しくモデルをトレーニングする</div>
+    <div class="aqarium-dashboard">
       <div
-        v-for="(menu, index) in menuList"
-        :key="index"
+        v-for="(menu, index) in modelTrain"
+        :key="'m' + index"
         class="card-container"
       >
         <router-link :to="menu.url">
           <el-card
             class="menu"
-            style="border: solid 1px #ebeef5; width: 360px; height: 200px; border-left: 5px solid rgba(26, 191, 213, 0.7);"
+            style="border: solid 1px #ebeef5; width: 400px; height: 200px; border-left: 5px solid rgba(26, 191, 213, 0.7);"
           >
             <div class="menu-name">
-              <icon
-                v-if="menu.category"
-                :name="menu.category"
-                scale="1.5"
-                class="menu-icon"
-              />
               {{ menu.name }}
             </div>
             <div
@@ -26,6 +21,69 @@
               style="padding: 10px; font-size: 14px;"
             >
               {{ menu.description }}
+            </div>
+            <div class="menu-start" style="padding: 20px; font-size: 14px;">
+              <icon :name="iconname" scale="1.5" class="menu-icon" />
+              開始
+            </div>
+          </el-card>
+        </router-link>
+      </div>
+    </div>
+    <div>推論をおこなう</div>
+    <div class="aqarium-dashboard">
+      <div
+        v-for="(menu, index) in inference"
+        :key="'i' + index"
+        class="card-container"
+      >
+        <router-link :to="menu.url">
+          <el-card
+            class="menu"
+            style="border: solid 1px #ebeef5; width: 400px; height: 200px; border-left: 5px solid rgba(26, 191, 213, 0.7);"
+          >
+            <div class="menu-name">
+              {{ menu.name }}
+            </div>
+            <div
+              class="menu-description"
+              style="padding: 10px; font-size: 14px;"
+            >
+              {{ menu.description }}
+            </div>
+            <div class="menu-start" style="padding: 20px; font-size: 14px;">
+              <icon :name="iconname" scale="1.5" class="menu-icon" />
+              開始
+            </div>
+          </el-card>
+        </router-link>
+      </div>
+    </div>
+
+    <div>テンプレートを管理する</div>
+    <div class="aqarium-dashboard">
+      <div
+        v-for="(menu, index) in template"
+        :key="'t' + index"
+        class="card-container"
+      >
+        <router-link :to="menu.url">
+          <el-card
+            class="menu"
+            style="border: solid 1px #ebeef5; width: 400px; height: 200px; border-left: 5px solid rgba(26, 191, 213, 0.7);"
+          >
+            <div class="menu-name">
+              {{ menu.name }}
+            </div>
+            <div
+              class="menu-description"
+              style="padding: 10px; font-size: 14px;"
+            >
+              {{ menu.description }}
+            </div>
+            <div class="menu-start" style="padding: 20px; font-size: 14px;">
+              <icon :name="iconname" scale="1.5" class="menu-icon" />
+              開始
             </div>
           </el-card>
         </router-link>
@@ -47,6 +105,31 @@ export default {
   title: 'ダッシュボード',
   data() {
     return {
+      iconname: 'pl-arrow-right',
+      modelTrain: [
+        {
+          category: '',
+          name: '学習',
+          url: '',
+          description: 'モデルをトレーニングします。',
+        },
+      ],
+      inference: [
+        {
+          category: '',
+          name: '推論',
+          url: '',
+          description: '学習済みモデルの選択、データ選択を行い推論を行います。',
+        },
+      ],
+      template: [
+        {
+          category: '',
+          name: 'テンプレート管理',
+          url: '',
+          description: 'テンプレートの閲覧、編集、新規登録をします。',
+        },
+      ],
       unwatchLogin: undefined,
     }
   },
@@ -102,18 +185,27 @@ a {
 .menu-description {
   font-weight: lighter;
 }
+.menu-start {
+  font-weight: lighter;
 
-.dashboard {
+  position: absolute;
+  bottom: 0;
+
+  align-content: flex-end;
+}
+.aqarium-dashboard {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content: space-evenly;
+  justify-content: start;
   align-content: flex-start;
+  margin-bottom: 40px;
 }
 
 .card-container {
   float: left;
   margin: 20px 20px 10px 0;
+  position: relative;
 }
 
 .footer {
