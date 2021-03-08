20ead6c70991d0271226239a2715874d7a7e02dc
ozota1 <ozota1@users.noreply.github.com>
2021-03-08 15:47:05 +0900

    dockerビルド方法の変更

--- release-tools/build-docker/build.sh
+++ release-tools/build-docker/build.sh
@@ -9,8 +9,7 @@ readonly REPO_ROOT_DIR="$SCRIPT_DIR/../.."
 main(){
     docker build --network host -f $SCRIPT_DIR/web-api-Dockerfile -t kamonohashi/web-api:$TAG --build-arg version=$VERSION web-api
     docker build --network host -f $SCRIPT_DIR/cli-Dockerfile -t kamonohashi/cli:$TAG --build-arg version=$VERSION .
-    docker build --network host -f $SCRIPT_DIR/web-pages-Dockerfile -t kamonohashi/web-pages:$TAG web-pages
-    docker build --network host -f $SCRIPT_DIR/web-pages-aquarium-Dockerfile -t kamonohashi/web-pages-aquarium:$TAG web-pages-aquarium
+    docker build --network host -f $SCRIPT_DIR/web-pages-Dockerfile -t kamonohashi/web-pages:$TAG .
 }
 
 # このスクリプトをどこから実行しても大丈夫なように
