93e10ad56883230f065edbb997f25f61d353e333
4packs <40011990+4packs@users.noreply.github.com>
2021-03-10 19:05:28 +0900

    テンプレートバージョン化

---
+++ web-api/platypus/platypus/ApiModels/TemplateApiModels/CreateInputModel2.cs
@@ -0,0 +1,30 @@
+﻿using Nssol.Platypus.Infrastructure;
+using System.Collections.Generic;
+using Nssol.Platypus.ApiModels.Components;
+using Nssol.Platypus.Controllers.Util;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nssol.Platypus.ApiModels.TemplateApiModels
+{
+    /// <summary>
+    /// テンプレート作成の入力モデル
+    /// </summary>
+    public class CreateInputModel2
+    {
+        /// <summary>
+        /// 名前
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// メモ
+        /// </summary>
+        public string Memo { get; set; }
+
+        /// <summary>
+        /// アクセスレベル
+        /// </summary>
+        public TemplateAccessLevel AccessLevel { get; set; }
+    }
+}
