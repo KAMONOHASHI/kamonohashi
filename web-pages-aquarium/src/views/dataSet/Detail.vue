22f5e5e611756f1b4939587ceb50eb9e490ff3b6
Reiw98 <wakamatsu.rei.hr8@jp.nssol.nipponsteel.com>
2021-12-06 09:07:20 +0900

    アクアリウムのデータセットでkamonohashiのデータを選択する際のレイアウト崩れを修正

--- web-pages-aquarium/src/views/dataSet/Detail.vue
+++ web-pages-aquarium/src/views/dataSet/Detail.vue
@@ -205,7 +205,7 @@
               <kqi-display-error :error="error" />
               <el-row>
                 <div
-                  style="width:80%;height:250px;padding:20px;border:1px solid #CCC;border-radius:5px;margin-top:5px"
+                  style="width:80%;height:250px;padding:20px;border:1px solid #CCC;border-radius:5px;margin-top:5px;overflow-y:auto;"
                 >
                   <el-checkbox-group v-model="checkList">
                     <div v-for="item in allDatas" :key="item.id">
