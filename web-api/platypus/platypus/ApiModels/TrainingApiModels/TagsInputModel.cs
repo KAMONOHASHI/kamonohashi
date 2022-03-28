9e5a90cd52233dabe439b8cd449f4fe55ccf8bde
netresj <ariari.hiroki.1219@gmail.com>
2022-03-28 14:49:55 +0900

    タグの一括付与・削除APIの入力モデルにコメントを付与

--- web-api/platypus/platypus/ApiModels/TrainingApiModels/TagsInputModel.cs
+++ web-api/platypus/platypus/ApiModels/TrainingApiModels/TagsInputModel.cs
@@ -6,9 +6,15 @@ namespace Nssol.Platypus.ApiModels.TrainingApiModels
 {
     public class TagsInputModel
     {
+        /// <summary>
+        /// タグを付与、または、削除したい対象の学習履歴のId
+        /// </summary>
         [Required]
         public IEnumerable<long> Id { get; set; }
 
+        /// <summary>
+        /// 付与、または、削除したいタグ
+        /// </summary>
         [Required]
         public IEnumerable<string> Tags { get; set; }
     }
