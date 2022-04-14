8d9ef5078516f7cfa399a25f998a0d3a160031f3
ota-hiroko <ota.hiroko.53d@jp.nssol.nipponsteel.com>
2022-04-14 18:57:48 +0900

    UesrsテーブルにDisplayName追加

---
+++ web-api/platypus/platypus/Migrations/20220413055042_v4.0.1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nssol.Platypus.Migrations
+{
+    public partial class v401 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "Users");
+        }
+    }
+}
