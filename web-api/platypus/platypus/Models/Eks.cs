fb74d419789ba16d8c585481c3eb815d64b185f5
netresj <ariari.hiroki.1219@gmail.com>
2022-07-25 11:18:24 +0900

    退避

---
+++ web-api/platypus/platypus/Models/Eks.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Nssol.Platypus.Models
+{
+    /// <summary>
+    /// EKSの登録情報
+    /// </summary>
+    public class Eks : ModelBase
+    {
+        /// <summary>
+        /// 識別名
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// メモ
+        /// </summary>
+        public string Memo { get; set; }
+
+        /// <summary>
+        /// ホスト名
+        /// </summary>
+        [Required]
+        public string HostName { get; set; }
+
+        /// <summary>
+        /// ポート番号
+        /// </summary>
+        [Required]
+        public int PortNumber { get; set; }
+
+        /// <summary>
+        /// API key (k8sの認証キー)
+        /// </summary>
+        [Required]
+        public string ApiKey { get; set; }
+    }
+}
